f3ad542da93cd73f032a53c2ad0a8f986d2c8a38
Nick Hong <me@nickhong.xyz>
2023-02-13 17:50:36 -0500

    Switch from using label to children on button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,9 @@
 export interface ButtonProps {
-  label: string;
-  primary: boolean;
-  size: "small" | "medium" | "large";
+  children?: React.ReactNode;
+  primary?: boolean;
+  size?: "small" | "medium" | "large";
 }
 
-export default function Button ({ label, primary, size }: ButtonProps) {
-  return <button className="bg-red-500 p-2">{label}</button>;
+export default function Button ({ children, primary, size }: ButtonProps) {
+  return <button className="bg-red-500 p-2">{children}</button>;
 };
