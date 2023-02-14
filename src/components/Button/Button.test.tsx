adba693959877885d6dafff913716461f63c9b80
Nick Hong <me@nickhong.xyz>
2023-02-13 21:38:10 -0500

    Add simple click test for button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { faker } from "@faker-js/faker";
+import userEvent from "@testing-library/user-event";
 
 import Button from "./";
+import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
+
+let user: UserEvent;
+
+beforeAll(() => {
+  user = userEvent.setup();
+});
 
 describe("<Button />", () => {
-  test("rendered text", () => {
+  test("renders text children", () => {
     const buttonLabel = faker.lorem.words();
     render(<Button>{buttonLabel}</Button>);
     expect(screen.getByText(buttonLabel)).toBeDefined();
+  });
+
+  test("onclick triggers on click", async () => {
+    const label = faker.lorem.words();
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>{label}</Button>);
+    await user.click(screen.getByRole("button", { name: label }));
+    expect(onClick.mock.calls).toHaveLength(1);
   });
 });
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,9 +2,19 @@ import React from "react";
 export interface ButtonProps {
   children?: React.ReactNode;
   primary?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   size?: "small" | "medium" | "large";
 }
 
-export default function Button({ children, primary, size }: ButtonProps) {
-  return <button className="bg-red-500 p-2">{children}</button>;
+export default function Button({
+  children,
+  onClick,
+  primary,
+  size
+}: ButtonProps) {
+  return (
+    <button className="bg-red-500 p-2" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
