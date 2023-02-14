00d265a59e0bbb3e9fe8ee5a954cba06d6d19413
Nick Hong <me@nickhong.xyz>
2023-02-13 21:15:01 -0500

    Add prettier test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
 import Button from "./";
 
